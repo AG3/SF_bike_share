3efc2e892e329fb43bea49534e0d0247523b2361
JiaYi Zhang <jiayizhang@JiaYideMacBook-Air.local>
2016-12-21 23:40:24 -0800

    LSTM

--- predict.py
+++ predict.py
@@ -9,21 +9,27 @@ avr = 0
 
 train_X = []
 train_Y = []
-
+test_X = []
 ind = 0
 for i in infile:
 	train_X.append(ind)
+	test_X.append(ind)
 	y = int(i.split(' ')[1])
 	train_Y.append(y)
 	plt.scatter(ind,y)
 	ind+=1
 
+
+for i in range(ind, ind+20):
+	test_X.append(i)
+
 X_np = np.array(train_X).reshape(-1,1)
 Y_np = np.array(train_Y).reshape(-1,1)
-
+test_np = np.array(test_X).reshape(-1,1)
 
 poly = PolynomialFeatures(5)
-b = poly.fit_transform(X_np)
+b = poly.fit_transform(X_np)z
+test_b = poly.fit_transform(test_np)
 
 regr = linear_model.LinearRegression()
 
@@ -32,8 +38,8 @@ regr.fit(b, train_Y)
 print 'Coefficients:\n', regr.coef_
 print 'Intercept:\n', regr.intercept_
 
-res = regr.predict(b)
+res = regr.predict(test_b)
 
-plt.plot(train_X, res, color='blue',linewidth=3)
+plt.plot(test_X, res, color='blue',linewidth=3)
 
 plt.show()
